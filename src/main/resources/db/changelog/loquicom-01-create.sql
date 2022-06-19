f237137eefc1d81c865d60f6a0b3113b1579310c
Loquicom <no-reply@loquicom.fr>
2022-06-19 21:04:25 +0200

    Use UUID instead of Serial
    + Add not null if needed on foreign key

--- src/main/resources/db/changelog/loquicom-01-create.sql
+++ src/main/resources/db/changelog/loquicom-01-create.sql
@@ -3,7 +3,7 @@
 
 CREATE TABLE users
 (
-    us_id    SERIAL,
+    us_id    UUID,
     login    VARCHAR(500),
     password VARCHAR(255),
     email    VARCHAR(500),
@@ -12,66 +12,66 @@ CREATE TABLE users
 
 CREATE TABLE forecast
 (
-    fo_id   SERIAL,
+    fo_id   UUID,
     amount  DECIMAL,
     libelle VARCHAR(500),
     date    DATE,
-    ba_id   SERIAL,
-    sa_id   SERIAL,
+    ba_id   UUID,
+    sa_id   UUID,
     PRIMARY KEY (fo_id)
 );
 
 CREATE TABLE bank
 (
-    ba_id       SERIAL,
+    ba_id       UUID,
     name        VARCHAR(500),
     description TEXT,
     main        BOOLEAN,
-    us_id       SERIAL,
+    us_id       UUID NOT NULL,
     PRIMARY KEY (ba_id)
 );
 
 CREATE TABLE saving
 (
-    sa_id       SERIAL,
+    sa_id       UUID,
     name        VARCHAR(500),
     description TEXT,
-    us_id       SERIAL,
-    ba_id       SERIAL,
+    us_id       UUID NOT NULL,
+    ba_id       UUID,
     PRIMARY KEY (sa_id)
 );
 
 CREATE TABLE recurrent
 (
-    re_id   SERIAL,
+    re_id   UUID,
     amount  DECIMAL,
     libelle VARCHAR(500),
     freq    VARCHAR(5),
     date    DATE,
     next    DATE,
     active  BOOLEAN,
-    sa_id   SERIAL,
-    ba_id   SERIAL,
+    sa_id   UUID,
+    ba_id   UUID,
     PRIMARY KEY (re_id)
 );
 
 CREATE TABLE income
 (
-    in_id   SERIAL,
+    in_id   UUID,
     amount  DECIMAL,
     libelle VARCHAR(500),
     date    DATE,
     checked BOOLEAN,
-    sa_id   SERIAL,
-    ba_id   SERIAL,
-    ta_id   SERIAL,
-    ca_id   SERIAL,
+    sa_id   UUID,
+    ba_id   UUID,
+    ta_id   UUID,
+    ca_id   UUID,
     PRIMARY KEY (in_id)
 );
 
 CREATE TABLE category
 (
-    ca_id   SERIAL,
+    ca_id   UUID,
     name    VARCHAR(500),
     income  BOOLEAN,
     expense BOOLEAN,
@@ -80,7 +80,7 @@ CREATE TABLE category
 
 CREATE TABLE tag
 (
-    ta_id   SERIAL,
+    ta_id   UUID,
     name    VARCHAR(500),
     income  BOOLEAN,
     expense BOOLEAN,
@@ -89,14 +89,14 @@ CREATE TABLE tag
 
 CREATE TABLE expense
 (
-    ex_id   SERIAL,
+    ex_id   UUID,
     amount  DECIMAL,
     libelle VARCHAR(500),
     date    DATE,
     checked BOOLEAN,
-    ba_id   SERIAL,
-    sa_id   SERIAL,
-    ta_id   SERIAL,
-    ca_id   SERIAL,
+    ba_id   UUID,
+    sa_id   UUID,
+    ta_id   UUID,
+    ca_id   UUID,
     PRIMARY KEY (ex_id)
 );
