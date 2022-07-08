a6d0d8170af22e1ffa3c5a2eae127d4faeeabaf8
Loquicom <no-reply@loquicom.fr>
2022-07-08 17:56:03 +0200

    Test for root controller

--- src/test/kotlin/com/loqui/esy/entry/controller/ControllerTest.kt
+++ src/test/kotlin/com/loqui/esy/entry/controller/ControllerTest.kt
@@ -1,9 +1,12 @@
 package com.loqui.esy.entry.controller
 
+import com.loqui.esy.maker.makeUserDTO
+import com.loqui.esy.utils.JWTUtil
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpHeaders
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.servlet.MockMvc
 
@@ -13,9 +16,21 @@ import org.springframework.test.web.servlet.MockMvc
 abstract class ControllerTest {
 
     @Autowired
+    protected lateinit var jwtUtil: JWTUtil
+
+    @Autowired
     protected lateinit var mockMvc: MockMvc
 
     @Value("\${esy.api.root-path}")
     protected lateinit var rootPath: String
+
+    fun authHeader(): HttpHeaders {
+        val userDTO = makeUserDTO()
+        val token = jwtUtil.generate(userDTO)
+
+        val headers = HttpHeaders()
+        headers.add("Authorization", "Bearer $token")
+        return headers
+    }
 
 }
---
+++ src/test/kotlin/com/loqui/esy/entry/controller/RootControllerTest.kt
@@ -0,0 +1,62 @@
+package com.loqui.esy.entry.controller
+
+import com.loqui.esy.maker.LOGIN
+import org.junit.jupiter.api.Test
+import org.springframework.security.test.context.support.WithUserDetails
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+class RootControllerTest : ControllerTest() {
+
+    @Test
+    fun infoTest() {
+        mockMvc
+            .perform(
+                get("$rootPath/")
+            )
+            .andExpect(status().isOk)
+    }
+
+    @Test
+    fun pingTest() {
+        mockMvc
+            .perform(
+                get("$rootPath/ping")
+            )
+            .andExpect(status().isOk)
+            .andExpect(content().string("pong"))
+    }
+
+    @Test
+    fun sayTest() {
+        val word = "test"
+        mockMvc
+            .perform(
+                get("$rootPath/say/$word")
+            )
+            .andExpect(status().isOk)
+            .andExpect(content().string("Esy server: $word"))
+    }
+
+    @Test
+    @WithUserDetails(LOGIN)
+    fun authTest() {
+        mockMvc
+            .perform(
+                get("$rootPath/auth").headers(authHeader())
+            )
+            .andExpect(status().isOk)
+            .andExpect(content().string("You are authentified"))
+    }
+
+    @Test
+    fun authUnauthorizedTest() {
+        mockMvc
+            .perform(
+                get("$rootPath/auth")
+            )
+            .andExpect(status().isUnauthorized)
+    }
+
+}
