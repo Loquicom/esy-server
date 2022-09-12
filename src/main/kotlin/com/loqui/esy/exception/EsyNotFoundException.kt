edd6e4ce6cdfbead4efbd3b8a009e4ad0d446783
Loquicom <dev@loquicom.fr>
2022-09-12 15:22:09 +0200

    Add a not found exception
    + Correct HttpStatus for other exception

--- src/main/kotlin/com/loqui/esy/exception/EsyNotFoundException.kt
+++ src/main/kotlin/com/loqui/esy/exception/EsyNotFoundException.kt
@@ -3,16 +3,16 @@ package com.loqui.esy.exception
 import com.loqui.esy.data.wrapper.EsyError
 import org.springframework.http.HttpStatus
 
-open class EsySecurityException(
+class EsyNotFoundException(
     message: String? = null,
     code: Int = 0,
-    status: HttpStatus = HttpStatus.FORBIDDEN,
+    status: HttpStatus = HttpStatus.BAD_REQUEST,
     throwable: Throwable? = null
 ) : EsyException(message, code, status, throwable) {
 
     constructor(error: EsyError, throwable: Throwable? = null) : this(error.message, error.code, error.status, throwable)
 
-    constructor(message: String? = null, throwable: Throwable? = null) : this(message, 0, HttpStatus.FORBIDDEN, throwable)
+    constructor(message: String? = null, throwable: Throwable? = null) : this(message, 0, HttpStatus.BAD_REQUEST, throwable)
 
     constructor(message: String? = null, status: HttpStatus, throwable: Throwable? = null) : this(message, 0, status, throwable)
 
--- src/main/kotlin/com/loqui/esy/exception/EsyTokenException.kt
+++ src/main/kotlin/com/loqui/esy/exception/EsyTokenException.kt
@@ -6,13 +6,13 @@ import org.springframework.http.HttpStatus
 class EsyTokenException(
     message: String? = null,
     code: Int = 0,
-    status: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
+    status: HttpStatus = HttpStatus.UNAUTHORIZED,
     throwable: Throwable? = null
 ) : EsySecurityException(message, code, status, throwable) {
 
     constructor(error: EsyError, throwable: Throwable? = null) : this(error.message, error.code, error.status, throwable)
 
-    constructor(message: String? = null, throwable: Throwable? = null) : this(message, 0, HttpStatus.INTERNAL_SERVER_ERROR, throwable)
+    constructor(message: String? = null, throwable: Throwable? = null) : this(message, 0, HttpStatus.UNAUTHORIZED, throwable)
 
     constructor(message: String? = null, status: HttpStatus, throwable: Throwable? = null) : this(message, 0, status, throwable)
 
